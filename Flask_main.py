1daa6881de82730f5f1ecbaadd5c5e3e24712c99
spear011 <103097743+spear011@users.noreply.github.com>
2022-04-06 16:10:49 +0900

    Update Flask_main.py

--- Flask_main.py
+++ Flask_main.py
@@ -4,9 +4,20 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #decorator
 def index():
   return render_template('index.html')
+
+# index.html
+#<!doctype html>
+# <html>
+#   <h1>Home</h1>
+#   <p>This is home.</p>
+# </html>
+  
+# routing: 사용자의 접속 경로를 지정
+# rendering : 페이지를 그려내는것
+# 외부 api 정보 가져오기
 
 @app.route('/posts')
 def show_posts():
@@ -14,8 +25,9 @@ def show_posts():
   to_serve = response.json()
   return jsonify(to_serve)
 
-if __name__=='__main__':
-  app.run(host='0.0.0.0', port=6000, debug=True)
+# /todos -> "This is todos"
 
-# 프로젝트 시작시 한번만 $ pip install flask
-# shell 에서 app 시작 : $ python main.py
+if __name__=='__main__':
+  app.run(host='0.0.0.0', port=4000, debug=True)
+  # 프로젝트 첫 시작시(한번만) $ pip install flask
+  # shell에서 app 시작: $ python main.py
